e0a9151e33670f9385324c98b05c89da028130d2
Feisal Mohamed <feisalmib@gmail.com>
2022-03-29 23:48:59 +0300

    updates

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - char s char accept
+ * @s: string
+ * @accept: string containing acceptable bytes
+ * Return: s or null if not found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int p;
+
+	for (; *s != '\0' ; s++)
+	{
+		for (p = 0 ; *(accept + p) != '\0' ; p++)
+		{
+			if (*s == *(accept + p))
+			{
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
